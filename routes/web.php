16f4217fae9c3f2ba10c2a63bd6279eeb2d3525a
Nadyne14 <nadynerosalia1234@gmail.com>
2025-04-19 01:05:06 +0700

    LOGIN done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/test', function () {
+    return 'Route test berhasil!';
+});
